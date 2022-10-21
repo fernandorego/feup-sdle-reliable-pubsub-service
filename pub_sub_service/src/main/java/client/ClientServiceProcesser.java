5a0a19f7404fa29179eb63e7fd023f6f96fd6cd3
fernandorego <fernandoluisrego@gmail.com>
2022-10-21 23:12:00 +0100

    Add fault tolerance if client does not recieve answer in unsubscribe operation

--- pub_sub_service/src/main/java/client/ClientServiceProcesser.java
+++ pub_sub_service/src/main/java/client/ClientServiceProcesser.java
@@ -18,15 +18,25 @@ public class ClientServiceProcesser {
             System.err.println(replyMessage.getError_message());
             return;
         }
-        FileUtils.createDir(CLIENT_DIR_PATH);
         String file_path = CLIENT_DIR_PATH + subscribeMessage.getClientId();
-        try {
-            FileUtils.createFile(CLIENT_DIR_PATH + subscribeMessage.getClientId());
+        checkClientFile(file_path);
+
+        try (BufferedReader br = new BufferedReader(new FileReader(file_path))) {
+            String json;
+            String file = "";
+            while ((json = br.readLine()) != null) {
+                ClientState clientState = ClientState.jsonToState(json);
+                if (!clientState.getTopic().equals(subscribeMessage.getTopic())) {
+                    file = file.concat(json + "\n");
+                }
+            }
+            FileUtils.stringToFile(file, file_path);
+            FileUtils.appendStringToFile((new ClientState(subscribeMessage.getTopic(),replyMessage.getOffset())).stateToJson() + "\n", file_path);
         } catch (IOException e) {
             System.out.println(e.toString());
             System.exit(1);
         }
-        FileUtils.appendStringToFile((new ClientState(subscribeMessage.getTopic(),replyMessage.getOffset())).stateToJson() + "\n", file_path);
+
         System.out.println("Client with id: " + subscribeMessage.getClientId() + " subscribed topic: " + subscribeMessage.getTopic());
     }
 
